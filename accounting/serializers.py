14b5ac845ba8db8334114253d31c55970cd19565
hac8hi <8rado.andrianirina8@gmail.com>
2025-10-08 12:15:32 +0200

    refactor: rename fields in models and serializers for consistency; add new fields to TransactionLine

--- accounting/serializers.py
+++ accounting/serializers.py
@@ -5,16 +5,13 @@ from .models import Account, JournalEntry, TransactionLine
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['id', 'numero', 'intitule', 'classe', 'type', 'nature', 'solde_initial']
+        fields = ['id', 'numero', 'intitule', 'classe', 'type', 'nature', 'soldeInitial']
 
 # Serializer pour le modèle TransactionLine
 class TransactionLineSerializer(serializers.ModelSerializer):
-    account = AccountSerializer(read_only=True)
-    account_id = serializers.PrimaryKeyRelatedField(queryset=Account.objects.all(), source='account', write_only=True)
-
     class Meta:
         model = TransactionLine
-        fields = ['id', 'account', 'account_id', 'debit', 'credit', 'calculated_amount', 'percentage', 'nature']
+        fields = ['id', 'compte', 'accountNumber', 'accountName', 'debit', 'credit', 'calculatedAmount', 'percentage', 'nature']
 
 # Serializer pour le modèle JournalEntry, incluant les lignes de transaction
 class JournalEntrySerializer(serializers.ModelSerializer):
@@ -22,13 +19,16 @@ class JournalEntrySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = JournalEntry
-        fields = ['id', 'date', 'libelle', 'reference', 'numero_ecriture', 'nature', 'created_at', 'updated_at', 'lines']
+        fields = ['id', 'date', 'libelle', 'reference', 'numeroEcriture', 'nature', 'created_at', 'updated_at', 'lines']
 
     def create(self, validated_data):
         # Création d'une écriture avec ses lignes
         lines_data = validated_data.pop('lines')
         entry = JournalEntry.objects.create(**validated_data)
         for line_data in lines_data:
-            account = line_data.pop('account') if 'account' in line_data else None
-            TransactionLine.objects.create(journal_entry=entry, account=account,**line_data)
+            compte = line_data.pop('compte')
+            accountNumber = compte.split(" - ")[0]
+            accountName = compte.split(" - ")[1]
+            account = Account.objects.get(numero=accountNumber)
+            TransactionLine.objects.create(journal_entry=entry,account=account,accountNumber=accountNumber,accountName=accountName,**line_data)
         return entry
